1699d60db9478dec6cf0553bd1ff4f479b9a10b0
hallymah-backend <olakitanhalimat@gmail.com>
2025-11-20 00:13:26 +0100

    that

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -127,11 +127,12 @@ from datetime import timedelta
 
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES':[
-        'rest_framework.permissions.IsAuthenticated',
-    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
 
-    'DEFAULT_AUTHENTICATION_CLASSES':[
-        'rest_framework.authentication.SessionAuthentication'
-    ]
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
 }
