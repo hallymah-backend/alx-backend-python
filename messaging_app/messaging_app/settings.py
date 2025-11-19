17a64ef31bd797ade14970a0d88dc79cecd45520
hallymah-backend <olakitanhalimat@gmail.com>
2025-11-20 00:37:44 +0100

    how far

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -125,14 +125,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 from datetime import timedelta
 
-
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'chats.auth.CustomJWTAuthentication',
+    ]
 }
-
-# SIMPLE_JWT = {
-#     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
-#     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
-# }
