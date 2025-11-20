7db55f65df6dd45f93f892a9f95e1950798e5387
hallymah-backend <olakitanhalimat@gmail.com>
2025-11-20 01:59:01 +0100

    permit

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -1,7 +1,20 @@
 from rest_framework import serializers
-from .models import Message
+from .models import Message, Conversation
+from django.contrib.auth.models import User
+
 
 class MessageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Message
         fields = '__all__'
+
+
+class ConversationSerializer(serializers.ModelSerializer):
+    participants = serializers.PrimaryKeyRelatedField(
+        many=True,
+        queryset=User.objects.all()
+    )
+
+    class Meta:
+        model = Conversation
+        fields = ['id', 'participants', 'created_at']
